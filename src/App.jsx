eed7e1b8fbac3cd6692a9dd7de3fee65b967ab0b
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-08 02:29:17 +0600

    Add header for all page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Router from './Router'
 
 const App = () => {
-  return <Router />
+  return (
+    <div className="app-main">
+      <h1>Header!</h1>
+      <Router />
+    </div>
+  )
 }
 
 export default App
