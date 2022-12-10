8395047aa0ef3af3efb03b4d666e3779806c8b0c
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-10 11:19:59 +0600

    Validation of form inputs.

--- src/pages/StreamCreate.jsx
+++ src/pages/StreamCreate.jsx
@@ -41,6 +41,20 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {}
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title'
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a title'
+  }
+
+  return errors
+}
+
 export default reduxForm({
   form: 'streamCreate',
 })(StreamCreate)
