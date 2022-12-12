5d9f10d57d71c6e7757a8e11014e1a90d49c64a1
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-12 23:52:02 +0600

    Linking to stream creation.

--- src/pages/StreamList.jsx
+++ src/pages/StreamList.jsx
@@ -4,17 +4,32 @@ import { connect } from 'react-redux'
 import { fetchStreams } from '../store/streams'
 
 const StreamList = (props) => {
+  console.log(props)
   useEffect(() => {
     props.fetchStreams()
   }, [])
+
+  const renderedAdmin = (stream) => {
+    if (stream.userId) {
+      return (
+        <div className="right floated content">
+          <button className="ui red button">Delete</button>
+          <button className="ui primary button">Edit</button>
+        </div>
+      )
+    }
+  }
 
   const renderedList = () => {
     return props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {renderedAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
-          <div className="content">{stream.title}</div>
-          <div className="description">{stream.description}</div>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
         </div>
       )
     })
@@ -28,7 +43,10 @@ const StreamList = (props) => {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.entities.streams.list) }
+  return {
+    streams: Object.values(state.entities.streams.list),
+    currentUserId: state.auth.userId,
+  }
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList)
