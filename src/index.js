a04585bf42dfa2d048076743c7e0d7874e97b3f0
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-13 19:53:25 +0600

    Refactoring the history object.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createBrowserHistory } from 'history'
 
 import App from './App'
 import configureStore from './store/configureStore'
+import history from './history'
 
 const store = configureStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const history = createBrowserHistory()
-
 root.render(
-  <BrowserRouter history={history}>
+  <BrowserRouter router={history}>
     <Provider store={store}>
       <App />
     </Provider>
