e3d260aca6e306183c65b8626a25672177981a5f
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-16 09:00:40 +0600

    Setting the initial value.

--- src/pages/StreamEdit.jsx
+++ src/pages/StreamEdit.jsx
@@ -1,21 +1,44 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { fetchStream } from '../store/streams'
+import { useParams, useNavigate } from 'react-router-dom'
+import StreamForm from '../components/StreamForm'
+import { editStream, fetchStream } from '../store/streams'
+
 const StreamEdit = (props) => {
+  const navigate = useNavigate()
   const { streamId } = useParams()
 
   useEffect(() => {
     props.fetchStream(streamId)
   }, [])
 
-  console.log(props)
+  useEffect(() => {
+    props.editStream(streamId)
+  }, [])
+
+  const onSubmit = (formValues) => {
+    console.log(formValues)
+    // props.createStream(formValues, navigate)
+  }
+
   if (!props.stream) return <div>Loading...</div>
-  return <div>{props.stream.title}</div>
+  return (
+    <div>
+      <h3>Edit a Stream</h3>
+
+      <StreamForm
+        initialValues={{
+          title: props.stream.title,
+          description: props.stream.description,
+        }}
+        onSubmit={onSubmit}
+      />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
   return { stream: state.entities.streams.stream }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { editStream, fetchStream })(StreamEdit)
