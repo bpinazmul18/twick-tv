9d3e3703c2a30b38b3d81d261a8158e651db7174
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-16 10:15:40 +0600

    Hiding a modal.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Modal = (props) => {
+  const navigate = useNavigate()
+
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">dfjklsjdafklajsd</div>
+    <div
+      onClick={() => navigate('/')}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete a Stream</div>
+        <div className="content">
+          <p>Are you sure?</p>
+        </div>
+        <div className="actions">
+          <div className="ui button primary">Delete</div>
+          <Link to={'/'} className="ui cancel button">
+            Cancel
+          </Link>
+        </div>
+      </div>
     </div>,
     document.querySelector('#modal')
   )
