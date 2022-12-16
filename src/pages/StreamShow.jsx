574634676b2a03cbbb8a064a0cabb37e3f0b403b
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-16 21:33:45 +0600

    Implementing FLV JS.

--- src/pages/StreamShow.jsx
+++ src/pages/StreamShow.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, createRef } from 'react'
 import { connect } from 'react-redux'
 import { fetchStream } from '../store/streams'
 import { useParams } from 'react-router-dom'
 
 const StreamShow = (props) => {
+  const videoRef = createRef()
   const { streamId } = useParams()
 
   useEffect(() => {
@@ -13,6 +14,7 @@ const StreamShow = (props) => {
   if (!props.stream) return <div>Loading...</div>
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       <h1>{props.stream.title}</h1>
       <h3>{props.stream.description}</h3>
     </div>
