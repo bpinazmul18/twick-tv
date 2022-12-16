dfabeb0cc513932e7910d247a137a288d9bb40c7
Nazmul Haque <bpinazmul18@gmail.com>
2022-12-16 21:59:39 +0600

    Optional player building.

--- src/pages/StreamShow.jsx
+++ src/pages/StreamShow.jsx
@@ -7,21 +7,24 @@ import { useParams } from 'react-router-dom'
 const StreamShow = (props) => {
   const videoRef = createRef()
   const { streamId } = useParams()
-
+  console.log(videoRef)
   useEffect(() => {
     props.fetchStream(streamId)
-  }, [])
+    buildVidoePlayer()
+  }, [streamId])
 
-  useEffect(() => {
+  const buildVidoePlayer = () => {
     const player = flb.createPlayer({
       type: 'flb',
       url: `http://localhost:8000/live/${streamId}.flv`,
     })
 
+    if (!player || !props.stream) return null
+
     player.attachMediaElement(videoRef.current)
     player.load()
     player.play()
-  }, [])
+  }
 
   if (!props.stream) return <div>Loading...</div>
   return (
